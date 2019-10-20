baa5596b48f7b39f1ebf65101ace24adfe3eb519
cutsea110 <cutsea110@gmail.com>
2019-10-20 10:08:58 +0900

    implement hylo

--- Hylo.hs
+++ Hylo.hs
@@ -9,3 +9,23 @@ fold f g (Node ts) = g (map (fold f g) ts)
 unfold :: (b -> Bool) -> (b -> a) -> (b -> [b]) -> b -> Tree a
 unfold p v h x = if p x then Leaf (v x)
                  else Node (map (unfold p v h) (h x))
+
+data LTree a = LLeaf a | LNode a [LTree a] deriving (Show, Eq)
+
+fill :: (a -> b) -> ([b] -> b) -> Tree a -> LTree b
+fill f g = fold (lleaf f) (lnode g)
+
+lleaf f x = LLeaf (f x)
+lnode g ts = LNode (g (map label ts)) ts
+
+label (LLeaf x) = x
+label (LNode x ts) = x
+
+hylo :: ([a] -> b) -> ([b] -> b) -> ([a] -> [[a]]) -> [a] -> b
+hylo f g h = fold f g . mkTree h
+
+mkTree h = unfold single id h
+
+single [] = False
+single (x:[]) = True
+single (x:xs) = False
