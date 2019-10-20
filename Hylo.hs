aa05e4caadb0c8bb1070de0bfb8079a3707e67fa
cutsea110 <cutsea110@gmail.com>
2019-10-20 10:05:16 +0900

    update new style

--- Hylo.hs
+++ Hylo.hs
@@ -2,16 +2,10 @@ module Hylo where
 
 data Tree a = Leaf a | Node [Tree a] deriving (Show, Eq)
 
-fold :: (Either a [b] -> b) -> Tree a -> b
-fold f t = case t of
-  Leaf x -> f (Left x)
-  Node ts -> f (Right (map (fold f) ts))
-
-unfold :: (b -> Either a [b]) -> b -> Tree a
-unfold g x = case g x of
-  Left y -> Leaf y
-  Right xs -> Node (map (unfold g) xs)
-
-hylo f g x = case g x of
-  Left y -> f (Left y)
-  Right xs -> f (Right (map (hylo f g) xs))
+fold :: (a -> b) -> ([b] -> b) -> Tree a -> b
+fold f g (Leaf x) = f x
+fold f g (Node ts) = g (map (fold f g) ts)
+  
+unfold :: (b -> Bool) -> (b -> a) -> (b -> [b]) -> b -> Tree a
+unfold p v h x = if p x then Leaf (v x)
+                 else Node (map (unfold p v h) (h x))
