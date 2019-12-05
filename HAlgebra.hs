acf5bd1b4238808e3e83c81b5bf6823a4fe07157
cutsea110 <cutsea110@gmail.com>
2019-12-05 10:00:36 +0900

    intro by using List and ListF

--- HAlgebra.hs
+++ HAlgebra.hs
@@ -22,3 +22,25 @@ import qualified Prelude
 import           Control.Category
 import           Data.Kind
 
+--------------------------------------------------
+
+data List a = Nil | Cons a (List a)
+
+data ListF a r = NilF | ConsF a r
+
+cataList :: (ListF a b -> b) -> (List a -> b)
+cataList f = go
+  where
+    go Nil = f NilF
+    go (Cons x xs) = f (ConsF x (go xs))
+
+deriving instance Functor (ListF a)
+
+newtype Fix f = In
+  { out :: f (Fix f)
+  }
+
+cata :: Functor f => (f b -> b) -> (Fix f -> b)
+cata f = go
+  where
+    go = f . fmap go . out
