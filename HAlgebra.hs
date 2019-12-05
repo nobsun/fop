54279d8cc2721a7e44899d197336c7f7c9d56d85
cutsea110 <cutsea110@gmail.com>
2019-12-05 10:17:38 +0900

    extend Expr

--- HAlgebra.hs
+++ HAlgebra.hs
@@ -44,3 +44,49 @@ cata :: Functor f => (f b -> b) -> (Fix f -> b)
 cata f = go
   where
     go = f . fmap go . out
+
+--------------------------------------------------
+
+type VarName = String
+
+{--
+-- | This is not good.
+--   because, when we'd like to add a new constructor, for instance, BoolLit,
+--   then Expr couldn't include that.
+--
+data Expr
+  = LamAbs VarName Expr
+  | App Expr Expr
+  | Var VarName
+  | NumLit Int
+
+--}
+
+data ExprF r
+  = LamAbsF VarName r
+  | AppF r r
+  | VarF VarName
+  | NumLitF Int
+  deriving Functor
+
+type Expr = Fix ExprF
+
+data (f :+: g) a = InL (f a) | InR (g a)
+  deriving Functor
+
+data BoolF r
+  = BoolLitF Bool
+  deriving Functor
+
+-- | Is this?
+type ExprExtF = ExprF :+: BoolF
+type ExprExt = Fix ExprExtF
+
+data CofreeF f a r = Cofree a (f r)
+  deriving Functor
+
+type ExprWithAnn a = Fix (CofreeF ExprF a)
+
+-- cofree annotation
+type Cofree f a = Fix (CofreeF f a)
+
