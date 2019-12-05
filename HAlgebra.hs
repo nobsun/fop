4de0e82f13a5cff20f6234e92eee226e778d549c
cutsea110 <cutsea110@gmail.com>
2019-12-05 09:52:40 +0900

    Recursion Schemes for Higher Algebra
    ref.) https://qiita.com/Mizunashi_Mana/items/d73162057ff276a3238a

---
+++ HAlgebra.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+module HAlgebra where
+
+import           Prelude hiding ((.), id)
+import qualified Prelude
+import           Control.Category
+import           Data.Kind
+
