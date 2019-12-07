bbd089be8c288de25dddf5ec2dff31849bfd1b39
cutsea110 <cutsea110@gmail.com>
2019-12-08 08:51:51 +0900

    fix point over sum type

--- DataTypesALaCarte.hs
+++ DataTypesALaCarte.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE TypeOperators #-}
 -- | ref.) http://www.cs.ru.nl/~W.Swierstra/Publications/DataTypesALaCarte.pdf
 module DataTypesALaCarte where
-
+{--
 data Expr = Val Int | Add Expr Expr
 
 eval :: Expr -> Int
@@ -10,4 +11,28 @@ eval (Add x y) = eval x + eval y
 render :: Expr -> String
 render (Val x) = show x
 render (Add x y) = "(" ++ render x ++ "+" ++ render y ++ ")"
+--}
 
+data Val e = Val Int
+type IntExpr = Expr Val
+data Add e = Add e e
+type AddExpr = Expr Add
+data Expr f = In (f (Expr f))
+
+data (f :+: g) e = Inl (f e) | Inr (g e)
+
+addExample :: Expr (Val :+: Add)
+addExample = In (Inr (Add (In (Inl (Val 118))) (In (Inl (Val 1219)))))
+
+instance Functor Val where
+  fmap f (Val x) = Val x
+
+instance Functor Add where
+  fmap f (Add e1 e2) = Add (f e1) (f e2)
+
+instance (Functor f, Functor g) => Functor (f :+: g) where
+  fmap f (Inl e1) = Inl (fmap f e1)
+  fmap f (Inr e2) = Inr (fmap f e2)
+
+foldExpr :: Functor f => (f a -> a) -> Expr f -> a
+foldExpr f (In t) = f (fmap (foldExpr f) t)
