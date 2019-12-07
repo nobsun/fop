ef18be0ea11af9820f9812659fc41eb7f0c89381
cutsea110 <cutsea110@gmail.com>
2019-12-08 08:32:44 +0900

    straightforward Expr

--- DataTypesALaCarte.hs
+++ DataTypesALaCarte.hs
@@ -1,2 +1,13 @@
+-- | ref.) http://www.cs.ru.nl/~W.Swierstra/Publications/DataTypesALaCarte.pdf
 module DataTypesALaCarte where
+
+data Expr = Val Int | Add Expr Expr
+
+eval :: Expr -> Int
+eval (Val x) = x
+eval (Add x y) = eval x + eval y
+
+render :: Expr -> String
+render (Val x) = show x
+render (Add x y) = "(" ++ render x ++ "+" ++ render y ++ ")"
 
