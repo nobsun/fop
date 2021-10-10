8d705271f8ed4841c2414029eeee7a67ce5eff82
cutsea110 <cutsea110@gmail.com>
2021-10-10 23:56:46 +0900

    cosme

--- ICBICSort.hs
+++ ICBICSort.hs
@@ -7,16 +7,14 @@ debug = True
 tracing :: Show a => a -> a
 tracing wk = if debug then trace (show wk) wk else wk
 
-sample = [1,3,2,5,4,7,6,0]
-
 -- I Can't Believe It Can Sort Algorithm.
-icbics :: (Ord a, Show a) => [a] -> [a]
+icbics :: (Show a, Ord a) => [a] -> [a]
 icbics xs = go ([], xs)
   where
     go (xs, []) = xs
     go xys      = go (swapper xys)
 
-swapper :: (Show a, Ord a) => ([a],  [a]) -> ([a], [a])
+swapper :: (Show a, Ord a) => ([a], [a]) -> ([a], [a])
 swapper (xs, y:ys) = tracing tmp
   where
     tmp = (zs++[w], ws)
@@ -28,3 +26,5 @@ swp (x:xs) = case span (<x) xs of
   (xs',  []) -> (x, xs)
   (xs', ys') -> let (z, zs) = swp ys'
                 in  (z, xs'++[x]++zs)
+
+sample = [1,3,2,5,4,7,6,0]
