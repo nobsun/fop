b010ce80e6d4de4f308f8e8d01375e8caefabf6c
cutsea110 <cutsea110@gmail.com>
2021-10-11 01:39:36 +0900

    rename tracing as ($?)

--- ICBICSort.hs
+++ ICBICSort.hs
@@ -5,16 +5,16 @@ import Debug.Trace (trace)
 
 debug = True
 
-tracing :: Show a => (a -> b) -> a -> b
-tracing f x = if debug then trace (show x) (f x) else f x
+($?) :: Show a => (a -> b) -> a -> b
+f $? x = if debug then trace (show x) (f x) else f x
 
 -- I Can't Believe It Can Sort Algorithm.
 icbics :: (Show a, Ord a) => [a] -> [a]
-icbics xs = tracing go ([], xs)
+icbics xs = go $? ([], xs)
   where go (xs, []) = xs
-        go xys      = tracing go (swapper xys)
+        go xys      = go $? (swapper xys)
 
-swapper :: (Show a, Ord a) => ([a], [a]) -> ([a], [a])
+swapper :: Ord a => ([a], [a]) -> ([a], [a])
 swapper (xs, y:ys) = (zs++[w], ws)
   where (z, zs) = swp (y:xs)
         (w, ws) = swp (z:ys)
