473b79da47523b86e7337363949e9592286f110d
cutsea110 <cutsea110@gmail.com>
2021-10-11 14:45:35 +0900

    remove unnecessary paren

--- ICBICSort.hs
+++ ICBICSort.hs
@@ -12,7 +12,7 @@ f $? x = if debug then trace (show x) (f x) else f x
 icbics :: (Show a, Ord a) => [a] -> [a]
 icbics xs = go $? ([], xs)
   where go (xs, []) = xs
-        go xys      = go $? (swapper xys)
+        go xys      = go $? swapper xys
 
 swapper :: Ord a => ([a], [a]) -> ([a], [a])
 swapper (xs, y:ys) = (zs++[w], ws)
