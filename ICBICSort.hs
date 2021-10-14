51dc8fee8f681b2a820a15e551b9a2e7268714a5
cutsea110 <cutsea110@gmail.com>
2021-10-14 17:24:44 +0900

    export list

--- ICBICSort.hs
+++ ICBICSort.hs
@@ -1,5 +1,8 @@
 -- ref.) https://arxiv.org/pdf/2110.01111.pdf
-module ICBICSort where
+module ICBICSort
+  ( sort
+  , sortBy
+  ) where
 
 import Debug.Trace (trace)
 
@@ -10,16 +13,22 @@ f $? x = if debug then trace (show x) (f x) else f x
 
 -- I Can't Believe It Can Sort Algorithm.
 sort :: (Show a, Ord a) => [a] -> [a]
-sort {-xs-} = sortBy (<) {-xs-}
+sort = sortBy (<)
 
 sortBy :: (Show a, Ord a) => (a -> a -> Bool) -> [a] -> [a]
 sortBy cmp xs = swapperBy cmp $? ([], xs)
+
+swapper :: (Show a, Ord a) => ([a], [a]) -> [a]
+swapper = swapperBy (<)
 
 swapperBy :: (Show a, Ord a) => (a -> a -> Bool) -> ([a], [a]) -> [a]
 swapperBy cmp (xs,   []) = xs
 swapperBy cmp (xs, y:ys) = swapperBy cmp $? (zs++[w], ws)
   where (z, zs) = swpBy cmp (y:xs)
         (w, ws) = swpBy cmp (z:ys)
+
+swp :: Ord a => [a] -> (a, [a])
+swp = swpBy (<)
 
 swpBy :: Ord a => (a -> a -> Bool) -> [a] -> (a, [a])
 swpBy cmp xxs@(x:_) = case break (x `cmp`) xxs of
